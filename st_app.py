90aabf7b0cfbcab9772e4e35efd65a2a27926035
Alec James <ajm.mex.poker@gmail.com>
2023-04-11 12:08:30 -0700

    'added players, cleaned app layout'

--- st_app.py
+++ st_app.py
@@ -31,33 +31,36 @@ from st_aggrid import AgGrid
 
 
 # Web-app title
-st.markdown("<h1 style='text-align: center; color: red;'>Mike Trout vs Clustered Pitchers</h1>", unsafe_allow_html=True)
-st.markdown("<h4 style='text-align: center; color: black;'>Using advanced pitch metric data from BaseballSavant, a KMeans Clustering model is used to accurately group pitchers. Then we compare Mike Trout's statistics vs each cluster to gain insight into pitcher preferences and predict future outcomes.  </h4>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Batter vs Clustered Pitchers</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: black;'>Using advanced pitch metric data from BaseballSavant, a KMeans Clustering model is used to accurately group pitchers. Then we compare Batter statistics vs each cluster to gain insight into pitcher preferences and predict future outcomes.  </h4>", unsafe_allow_html=True)
 ##################################################
 # Mike Trout vs Pitcher Career Numbers : df_trout
 ##################################################
+hitters = ['Shohei_Ohtani', 'Mike_Trout', 'Gio_Urshela', 'Hunter_Renfroe']
+hitter_choose = st.selectbox('Choose Batter', hitters)
 
-def load_trout():
+
+def load_hitter():
     # file_path = path.relpath("c:/Users/ajmme/app3/Mike_Trout.json")
-    with open("Mike_Trout.json") as f:
+    with open(f"{hitter_choose}.json") as f:
         data_str = f.read()
-        data_trout = json.loads(data_str)    
+        data_hitter = json.loads(data_str)    
     
-    trout = pd.DataFrame(data_trout)
-    trout_cols = ['pitcher','pitcherFirstName', 'pitcherLastName',
+    hitter = pd.DataFrame(data_hitter)
+    hitter_cols = ['pitcher','pitcherFirstName', 'pitcherLastName',
             'ab', 'h', 'avg', 'hr', 'so', 'bb', '2b', '3b', 
             'obp', 'slg', 'ops']
     
-    trout = trout[trout_cols]
-    trout['full_name'] = trout['pitcherFirstName'] + ' ' + trout['pitcherLastName']
-    trout['full_name'] = trout['full_name'].str.strip()
-    trout['slg'] = trout['slg'].astype(float)
-    trout['ops'] = trout['ops'].astype(float)
+    hitter = hitter[hitter_cols]
+    hitter['full_name'] = hitter['pitcherFirstName'] + ' ' + hitter['pitcherLastName']
+    hitter['full_name'] = hitter['full_name'].str.strip()
+    hitter['slg'] = hitter['slg'].astype(float)
+    hitter['ops'] = hitter['ops'].astype(float)
 
     
-    return trout
+    return hitter
 
-df_trout = load_trout()
+df_hitter = load_hitter()
 
 ##################################################
 ##################################################
@@ -94,48 +97,48 @@ df_metric = get_metric_data()
 # ##################################################
 
 # @st.cache_resource
-def data_2_cols():
-     col1, col2 = st.columns(2)
+# def data_2_cols():
+#      tab1, tab2 = st.tabs(['Cluster Distribution', 'Model Precision'])
      
-     metric_cols = df_metric.columns 
+#      metric_cols = df_metric.columns 
 
-    #  with col1:
-    #     # viz_cols = df_metric.columns
-    #     # viz_cole = metric_cols[0:55]
-    #     st.header('These are the predictors used for our KMeans model')
-    #     st.write(metric_cols[0:55])
+#     #  with col1:
+#     #     # viz_cols = df_metric.columns
+#     #     # viz_cole = metric_cols[0:55]
+#     #     st.header('These are the predictors used for our KMeans model')
+#     #     st.write(metric_cols[0:55])
 
-     with col1:
-        st.header('Distribution of Clusters')
-        cluster_distribution = plt.figure(figsize=(20, 20))
-        sns.set(font_scale = 6)
-        sns.countplot(x='Cluster', data=df_metric, palette ='deep').set(title='Pitcher Clusters')
-        st.pyplot(cluster_distribution)
+#      with tab1:
+#         st.header('Distribution of Clusters')
+#         cluster_distribution = plt.figure(figsize=(10, 10))
+#         sns.set(font_scale = 2)
+#         sns.countplot(x='Cluster', data=df_metric, palette ='deep').set(title='Pitcher Clusters')
+#         st.pyplot(cluster_distribution)
      
-     with col2: 
-        #  df_metric_col3_columns = df_metric.columns
-         metric_col3_columns = metric_cols[0:56]
-         df_col3 = df_metric[metric_col3_columns]
-         X_metric = df_col3.drop(['Cluster'],axis=1)
-         y_metric= df_col3[['Cluster']]
-         X_train_metric, X_test_metric, y_train_metric, y_test_metric =train_test_split(X_metric, y_metric, test_size=0.3)
+#      with tab2: 
+#         #  df_metric_tab3_columns = df_metric.columns
+#          metric_col3_columns = metric_cols[0:56]
+#          df_col3 = df_metric[metric_col3_columns]
+#          X_metric = df_col3.drop(['Cluster'],axis=1)
+#          y_metric= df_col3[['Cluster']]
+#          X_train_metric, X_test_metric, y_train_metric, y_test_metric =train_test_split(X_metric, y_metric, test_size=0.3)
          
-         kmeans_cluster_metric= DecisionTreeClassifier(criterion="entropy")
-         kmeans_cluster_metric.fit(X_train_metric, y_train_metric)
-         y_pred_metric = kmeans_cluster_metric.predict(X_test_metric)
-         tested_metric = loaded_model.predict(X_test_metric)
-         eval_metric = classification_report(y_test_metric, y_pred_metric, output_dict=True) 
-         eval_metric = pd.DataFrame(eval_metric).T
+#          kmeans_cluster_metric= DecisionTreeClassifier(criterion="entropy")
+#          kmeans_cluster_metric.fit(X_train_metric, y_train_metric)
+#          y_pred_metric = kmeans_cluster_metric.predict(X_test_metric)
+#          tested_metric = loaded_model.predict(X_test_metric)
+#          eval_metric = classification_report(y_test_metric, y_pred_metric, output_dict=True) 
+#          eval_metric = pd.DataFrame(eval_metric).T
 
-         st.header('Classification Accuracy')
+#          st.header('Classification Accuracy')
         
-         st.dataframe(eval_metric[['precision','f1-score']],400,400)
-         st.text('''Refresh page for different data splits 
-        resulting in different f1-scores''')
+#          st.dataframe(eval_metric[['precision','f1-score']],400,400)
+#          st.text('''Refresh page for different data splits 
+#         resulting in different f1-scores''')
      
-     return col1, col2 
+#      return tab1, tab2 
 
-col_viz = data_2_cols()
+# col_viz = data_2_cols()
 
 # tvs_clusters = trout_view_sum.index
 
@@ -149,7 +152,7 @@ def show_clusters():
     pyqldf = lambda q: sqldf(q, globals())
 
     cluster_query = """SELECT metric.Cluster, t.pitcher, ab, h, hr, avg, bb, so, obp 
-        FROM df_trout AS t 
+        FROM df_hitter AS t 
         LEFT JOIN df_metric AS metric 
         ON metric.full_name = t.pitcher 
         WHERE Cluster IS NOT NULL 
@@ -157,26 +160,26 @@ def show_clusters():
     
     # trout_clusters = pd.DataFrame(ps.sqldf(cluster_query, locals()))
 
-    trout_clusters = pyqldf(cluster_query)
+    hitter_clusters = pyqldf(cluster_query)
 
-    trout_agg_sum = trout_clusters.groupby('Cluster').sum()
-    trout_agg_sum['avg'] = trout_agg_sum['h'] / trout_agg_sum['ab']
+    hitter_agg_sum = hitter_clusters.groupby('Cluster').sum()
+    hitter_agg_sum['avg'] = hitter_agg_sum['h'] / hitter_agg_sum['ab']
 
     # tvs_clusters = trout_agg_sum.index
 
     # cluster = st.selectbox('Choose Cluster', tvs_clusters)
 
-    col21, col22 = st.columns(2)
+    tab21, tab22 = st.tabs(['Career vs Pitcher', 'Career vs Cluster'])
 
 
-    with col21:
-        st.markdown("<h4 style='text-align: center; color: black;'>Trout's career statistics vs pitchers in each cluster</h4>", unsafe_allow_html=True)
+    with tab21:
+        st.markdown("<h4 style='text-align: center; color: black;'>Hitter's career statistics vs pitchers in each cluster</h4>", unsafe_allow_html=True)
         # tvs_clusters = trout_view_sum.index
         # tvs_clusters = trout_clusters['Cluster']
 
         # cluster = st.selectbox('Choose Cluster', tvs_clusters)
         # pitcher_cluster_view = trout_clusters.iloc[[tvs_clusters]]
-        st.dataframe(trout_clusters)
+        st.dataframe(hitter_clusters)
         st.markdown("<p style='text-align: center; color: black;'>Full Screen option in top right of dataframe</p>", unsafe_allow_html=True)
 
         # AgGrid(trout_clusters)
@@ -189,8 +192,8 @@ def show_clusters():
         # st.write(pitcher_cluster_view, 325, 400)
 
     
-    with col22:
-        st.markdown("<h4 style='text-align: center; color: black;'>Trout's career statistics vs each cluster</h4>", unsafe_allow_html=True)
+    with tab22:
+        st.markdown("<h4 style='text-align: center; color: black;'>Hitter's career statistics vs each cluster</h4>", unsafe_allow_html=True)
         # trout_agg_sum = trout_clusters.groupby('Cluster').sum()
         # trout_agg_sum['avg'] = trout_agg_sum['h'] / trout_agg_sum['ab']
 
@@ -199,11 +202,11 @@ def show_clusters():
         # st.dataframe(tr_slg, 325,400)
         # trout_agg_sum['ops'] = trout_agg_sum['ops'] / trout_agg_sum['ab']
 
-        st.dataframe(trout_agg_sum)
+        st.dataframe(hitter_agg_sum)
         st.markdown("<p style='text-align: center; color: black;'>Sort columns by left-click</p>", unsafe_allow_html=True)
 
 
-    return trout_agg_sum
+    return hitter_agg_sum
 
 # show_clusters()
 
@@ -212,31 +215,32 @@ def show_clusters():
 
 def density():
 
-    trout_agg_sum = show_clusters()
+    hitter_agg_sum = show_clusters()
 
-    # trout_agg_sum = show_clusters()
+    # hitter_agg_sum = show_clusters()
     
     st.header('Bayesian Updated Batting Avg vs Cluster')
 
-    clusters = trout_agg_sum.index
+    clusters = hitter_agg_sum.index
 
     clusters_choose = st.selectbox('Choose Cluster', clusters)
 
-    cluster_spec_view = trout_agg_sum.iloc[[clusters_choose]]
+    cluster_spec_view = hitter_agg_sum.iloc[[clusters_choose]]
 
-    # dropped_agg_sum = trout_agg_sum.drop([[clusters_choose]])
+    # dropped_agg_sum = hitter_agg_sum.drop([[clusters_choose]])
 
-    total_ab = trout_agg_sum['ab'].sum()
-    total_hit = trout_agg_sum['h'].sum()
+    total_ab = hitter_agg_sum['ab'].sum()
+    total_hit = hitter_agg_sum['h'].sum()
 
     cluster_ab = cluster_spec_view['ab']
     cluster_hit = cluster_spec_view['h']
 
     # prior a 
-    # a = (total_hit - cluster_hit)
-    # b = (total_ab - cluster_ab) - (total_hit - cluster_hit)
-    a = 124
-    b = 438 - 124
+    a = (total_hit - cluster_hit)
+    b = (total_ab - cluster_ab) - (total_hit - cluster_hit)
+
+    # a = 124
+    # b = 438 - 124
     # X_outcome = clustered_outcome_df.drop(['Cluster'],axis=1)
     theta_range = np.linspace(0, 1, 1000)
     theta_range_e = theta_range + 0.001 
@@ -283,16 +287,63 @@ def density():
 
     # likelihood = stats.binom.pmf(k = cluster_hit, n = cluster_ab, p = theta_range)
 
-    a1 = a
-    b1 = b + a
-    p1 = a1/b1
+    # a1 = a
+    # b1 = b + a
+    # p1 = a1/b1
 
-    pval = binom_test(cluster_hit[clusters_choose], cluster_ab[clusters_choose], p=p1, alternative='less')
-    pval300 = binom_test(cluster_hit[clusters_choose], cluster_ab[clusters_choose], p=.3, alternative='less')
-    pval333 = binom_test(cluster_hit[clusters_choose], cluster_ab[clusters_choose], p=.333, alternative='less')
-    st.write(f"The probability that Trout's avg vs this cluster is greater than his season avg of .281 is:  {round(pval, 4)*100}%")
-    st.write(f"The probability that Trout's avg vs this cluster is greater than 333 is: {round(pval333, 4)*100}%")
-    # st.write(cluster_hit[0], cluster_ab[0])
+    # # pval = binom_test(cluster_hit[clusters_choose], cluster_ab[clusters_choose], p=p1, alternative='less')
+    # # pval300 = binom_test(cluster_hit[clusters_choose], cluster_ab[clusters_choose], p=.3, alternative='less')
+    # # pval333 = binom_test(cluster_hit[clusters_choose], cluster_ab[clusters_choose], p=.333, alternative='less')
+    # pval = binom_test(cluster_hit, cluster_ab, p=p1, alternative='less')
+    # pval300 = binom_test(cluster_hit, cluster_ab, p=.3, alternative='less')
+    # pval333 = binom_test(cluster_hit, cluster_ab, p=.333, alternative='less')
+    # st.write(f"The probability that Trout's avg vs this cluster is greater than his season avg of .281 is:  {round(pval, 4)*100}%")
+    # st.write(f"The probability that Trout's avg vs this cluster is greater than 333 is: {round(pval333, 4)*100}%")
+    # # st.write(cluster_hit[0], cluster_ab[0])
 density()
 
+
+
+# def data_2_cols():
+#      tab1, tab2 = st.tabs(['Cluster Distribution', 'Model Precision'])
+     
+#      metric_cols = df_metric.columns 
+
+#     #  with col1:
+#     #     # viz_cols = df_metric.columns
+#     #     # viz_cole = metric_cols[0:55]
+#     #     st.header('These are the predictors used for our KMeans model')
+#     #     st.write(metric_cols[0:55])
+
+#      with tab1:
+#         st.header('Distribution of Clusters')
+#         cluster_distribution = plt.figure(figsize=(10, 10))
+#         sns.set(font_scale = 2)
+#         sns.countplot(x='Cluster', data=df_metric, palette ='deep').set(title='Pitcher Clusters')
+#         st.pyplot(cluster_distribution)
+     
+#      with tab2: 
+#         #  df_metric_tab3_columns = df_metric.columns
+#          metric_col3_columns = metric_cols[0:56]
+#          df_col3 = df_metric[metric_col3_columns]
+#          X_metric = df_col3.drop(['Cluster'],axis=1)
+#          y_metric= df_col3[['Cluster']]
+#          X_train_metric, X_test_metric, y_train_metric, y_test_metric =train_test_split(X_metric, y_metric, test_size=0.3)
+         
+#          kmeans_cluster_metric= DecisionTreeClassifier(criterion="entropy")
+#          kmeans_cluster_metric.fit(X_train_metric, y_train_metric)
+#          y_pred_metric = kmeans_cluster_metric.predict(X_test_metric)
+#          tested_metric = loaded_model.predict(X_test_metric)
+#          eval_metric = classification_report(y_test_metric, y_pred_metric, output_dict=True) 
+#          eval_metric = pd.DataFrame(eval_metric).T
+
+#          st.header('Classification Accuracy')
+        
+#          st.dataframe(eval_metric[['precision','f1-score']],400,400)
+#          st.text('''Refresh page for different data splits 
+#         resulting in different f1-scores''')
+     
+#      return tab1, tab2 
+
+# col_viz = data_2_cols()
 
